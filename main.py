54b63ff4dfd3c217862fdb9f115d2fa3a7bcedc1
Frieza <118882614+FriezaForce@users.noreply.github.com>
2023-03-23 20:07:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+import os
+import openai
+from dotenv import load_dotenv
+from colorama import Fore, Back, Style
+
+
+load_dotenv()
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+INSTRUCTIONS = """You are an AI assistant that is an expert in alcoholic beverages.
+You know about cocktails, wines, spirits and beers.
+You can provide advice on drink menus, cocktail ingredients, how to make cocktails, and anything else related to alcoholic drinks.
+If you are unable to provide an answer to a question, please respond with the phrase "I'm just a simple barman, I can't help with that."
+Please aim to be as helpful, creative, and friendly as possible in all of your responses.
+Do not use any external URLs in your answers. Do not refer to any blogs in your answers.
+Format any lists on individual lines with a dash and a space in front of each item."""
+
+TEMPERATURE = 0.5
+MAX_TOKENS = 500
+FREQUENCY_PENALTY = 0
+PRESENCE_PENALTY = 0.6
+
+MAX_CONTEXT_QUESTIONS = 10
+
+
+def get_response(instructions, previous_questions_and_answers, new_question):
+    """
+
+
+
+
+    """
+
+    messages = [
+        { "role": "system", "content": instructions },
+    ]
+
+    for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
+        messages.append({ "role": "user", "content": question })
+        messages.append({ "role": "assistant", "content": answer })
+
+    messages.append({ "role": "user", "content": new_question })
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS,
+        top_p=1,
+        frequency_penalty=FREQUENCY_PENALTY,
+        presence_penalty=PRESENCE_PENALTY,
+    )
+    return completion.choices[0].message.content
+
+
+def get_moderation(question):
+    """
+
+    """
+
+    errors = {
+        "hate": "Content that expresses, incites, or promotes hate based on race, gender, ethnicity, religion, nationality, sexual orientation, disability status, or caste.",
+        "hate/threatening": "Hateful content that also includes violence or serious harm towards the targeted group.",
+        "self-harm": "Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders.",
+        "sexual": "Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness).",
+        "sexual/minors": "Sexual content that includes an individual who is under 18 years old.",
+        "violence": "Content that promotes or glorifies violence or celebrates the suffering or humiliation of others.",
+        "violence/graphic": "Violent content that depicts death, violence, or serious physical injury in extreme graphic detail.",
+    }
+    response = openai.Moderation.create(input=question)
+    if response.results[0].flagged:
+
+        result = [
+            error
+            for category, error in errors.items()
+            if response.results[0].categories[category]
+        ]
+        return result
+    return None
+
+
+def main():
+    os.system("cls" if os.name == "nt" else "clear")
+
+    previous_questions_and_answers = []
+    while True:
+
+        new_question = input(
+            Fore.GREEN + Style.BRIGHT + "What can I get you?: " + Style.RESET_ALL
+        )
+
+        errors = get_moderation(new_question)
+        if errors:
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + "Sorry, you're question didn't pass the moderation check:"
+            )
+            for error in errors:
+                print(error)
+            print(Style.RESET_ALL)
+            continue
+        response = get_response(INSTRUCTIONS, previous_questions_and_answers, new_question)
+
+
+        previous_questions_and_answers.append((new_question, response))
+
+
+        print(Fore.CYAN + Style.BRIGHT + "Here you go: " + Style.NORMAL + response)
+
+
+if __name__ == "__main__":
+    main()
